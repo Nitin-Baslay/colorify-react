3430d80afee917275d45a734d68c2d204ede4152
Nitin Kumar <113196521+Nitin-Baslay@users.noreply.github.com>
2023-05-06 09:41:54 +0530

    Add files via upload

---
+++ src/components/Home.js
@@ -0,0 +1,105 @@
+import { ChromePicker } from "react-color";
+import { useState } from "react";
+import { useHistory } from "react-router-dom";
+import axios from "axios";
+import "./Home.css";
+
+const Home = () => {
+  let history = useHistory();
+  const [color, setColor] = useState("#fff");
+  const [getColor, setGetColor] = useState([]);
+  let Id = localStorage.getItem("userid");
+  const colorChangeHandler = (event) => {
+    setColor(event.hex);
+  };
+  const addColorHandler = () => {
+    let colorObj = {
+      color: color,
+    };
+    axios.post(
+      `https://colorify-676d0-default-rtdb.firebaseio.com/name/${Id}.json`,
+      colorObj
+    );
+  };
+  axios
+    .get(`https://colorify-676d0-default-rtdb.firebaseio.com/name/${Id}.json`)
+    .then((response) => {
+      let Colors = [];
+      for (let key in response.data) {
+        let objColor = {
+          color: response.data[key].color,
+          key: key,
+        };
+        Colors.push(objColor);
+      }
+      setGetColor(Colors);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+  const editHandler = (key) => {
+    let updatedcolorObj = {
+      color: color,
+    };
+    axios.put(
+      `https://colorify-676d0-default-rtdb.firebaseio.com/name/${Id}/${key}.json`,
+      updatedcolorObj
+    );
+  };
+  const deleteHandler = (key) => {
+    axios.delete(
+      `https://colorify-676d0-default-rtdb.firebaseio.com/name/${Id}/${key}.json`
+    );
+  };
+  const logoutHandler = () => {
+    history.replace("/");
+  };
+  return (
+    <div>
+      <h1 className="head">Welcome User</h1>
+      <button className="logout" onClick={logoutHandler}>
+        Logout
+      </button>
+      <div className="main">
+        <div className="colorMaster">
+          {getColor.map((ele) => {
+            return (
+              <div key={ele.key}>
+                <div
+                  style={{ background: `${ele.color}` }}
+                  className="colorbox"
+                >
+                  <button
+                    className="bttns"
+                    onClick={() => {
+                      editHandler(ele.key);
+                    }}
+                  >
+                    Edit
+                  </button>
+                  <button
+                    className="bttns"
+                    onClick={() => {
+                      deleteHandler(ele.key);
+                    }}
+                  >
+                    Delete
+                  </button>
+                </div>
+              </div>
+            );
+          })}
+        </div>
+        <ChromePicker
+          color={color}
+          onChange={colorChangeHandler}
+          className="colorPicker"
+        />
+        <button onClick={addColorHandler} className="addbttn">
+          Add Color
+        </button>
+      </div>
+    </div>
+  );
+};
+export default Home;
---
+++ src/components/Login.js
@@ -0,0 +1,74 @@
+import axios from "axios";
+import { useState } from "react";
+import "./Login.css";
+import { useHistory } from "react-router-dom";
+const Login = () => {
+  const history = useHistory();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const emailHandler = (event) => {
+    setEmail(event.target.value);
+  };
+  const passwordHandler = (event) => {
+    setPassword(event.target.value);
+  };
+  const signUpHandler = () => {
+    axios
+      .post(
+        "https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyAFIn174YoVA_ha5RGrDR6MmD3Oxh0L8iU",
+        { email: email, password: password, returnSecureToken: true }
+      )
+      .then((response) => {
+        alert(`${response.data.email} Registered Successfully `);
+      })
+      .catch((error) => {
+        alert(error.response.data.error.message);
+      });
+  };
+  const signInHandler = () => {
+    axios
+      .post(
+        "https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyAFIn174YoVA_ha5RGrDR6MmD3Oxh0L8iU",
+        { email: email, password: password, returnSecureToken: true }
+      )
+      .then((response) => {
+        history.replace("/home");
+        let userId = response.data.localId;
+        localStorage.setItem("userid", userId);
+      })
+      .catch((error) => {
+        alert(error.response.data.error.message);
+      });
+  };
+  return (
+    <div className="master">
+      <h1 className="head">
+        <span style={{ color: "red" }}>C</span>
+        <span style={{ color: "green" }}>o</span>
+        <span style={{ color: "orange" }}>l</span>
+        <span style={{ color: "pink" }}>o</span>
+        <span style={{ color: "blue" }}>r</span>
+        <span style={{ color: "yellow" }}>i</span>
+        <span style={{ color: "purple" }}>f</span>
+        <span style={{ color: "brown" }}>y</span>
+      </h1>
+      <div>
+        <label className="label label1">Username</label>
+        <input type={"email"} className="inp" onChange={emailHandler} />
+      </div>
+      <div>
+        <label className="label">Password</label>
+        <input type={"password"} className="inp" onChange={passwordHandler} />
+      </div>
+      <div className="buttonclass">
+        <button className="bttn" onClick={signUpHandler}>
+          Sign Up
+        </button>
+        <button className="bttn bttn1" onClick={signInHandler}>
+          Login
+        </button>
+      </div>
+    </div>
+  );
+};
+export default Login;
